69a40505e11e63d10b1889e77295a8e907c6a19e
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-03-17 15:19:06 +0100

    config: Add Lighthouse to CI

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -2,6 +2,11 @@ describe("Homepage", () => {
     it('displays a heading', () => {
         cy.visit("https://beta.gouv.fr/");
 
-        cy.get("h1").should("be.visible")
+        cy.findByRole("heading", { level: 1 }).should("be.visible")
+
+        cy.findByRole("searchbox").type("foo")
+        cy.findByRole("button", { name: "Rechercher" }).click()
+
+        cy.findByRole("heading", {level: 1}).should("have.text", "Résultats")
     })
 })
